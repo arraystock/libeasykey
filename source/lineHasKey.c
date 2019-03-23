bb2dafa2b4d39d0f0111d27ce49bdc5281469092
Jonathan Archer <jon@jonnyarcher.com>
2019-03-23 17:17:08 -0500

    Add function description

--- source/lineHasKey.c
+++ source/lineHasKey.c
@@ -5,6 +5,10 @@
 
 #include "easykey.h"
 
+/*
+This function reads the beginning of a line and checks if it starts with the key
+name and if the key name is followed by " = ".
+*/
 bool lineHasKey(const char *Line, const char *Key) {
   return (strncmp(Key, Line, strlen(Key)) == 0) &&
          (strncmp(&Line[strlen(Key)], " = ", 3) == 0);

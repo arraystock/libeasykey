97756fa73bee15732cf6db4ddaeb96c5cb9d5e9f
Jonathan Archer <jon@jonnyarcher.com>
2019-03-30 15:36:52 -0500

    Fix

--- source/findKey.c
+++ source/findKey.c
@@ -14,7 +14,8 @@ if it exists. If the key does not exist, then the function returns
 EK_KEY_NO_EXIST (-1).
 */
 long findKey(const char *Filename, ek_key Key) {
-  if (findSection(Filename, Key.Section) != EK_SECTION_NO_EXIST) {
+  long Pos = findSection(Filename, Key.Section);
+  if (Pos != EK_SECTION_NO_EXIST) {
     FILE *File;
     if (access(Filename, F_OK) != -1)
       File = fopen(Filename, "r+");
@@ -24,18 +25,15 @@ long findKey(const char *Filename, ek_key Key) {
     char *Line = (char *)malloc(255 * sizeof(char));
     size_t Len = 0;
 
+    fseek(File, Pos, SEEK_SET);
+
     // Read in every line until we find the key.
     while (getline(&Line, &Len, File) != -1) {
       // Make sure we don't read into the next section.
-      if (isSection(Line)) {
+      if (isSection(Line) && !isSectionNamed(Line, Key.Section))
         return EK_KEY_NO_EXIST;
-        break;
-      }
-      if (lineHasKey(Line, Key.Name)) {
-        // Set the position.
+      if (lineHasKey(Line, Key.Name))
         return ftell(File) - strlen(Line);
-        break;
-      }
     }
     free(Line);
     fclose(File);
